60957439bda861f0a42c257483589e3ebc153364
jjunlob074 <jjunlob074@g.educaand.es>
2024-05-24 04:17:45 +0200

    implemento pagina cima, ajusto maquetaciones , añado animate.css y corrijo bugs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,14 @@
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import { defineConfig } from "astro/config";
+import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://astro-moon-landing.netlify.app/",
   integrations: [tailwind(), icon()],
+  output: 'server',
+  adapter: node({
+    mode: 'standalone',
+  }),
 });
