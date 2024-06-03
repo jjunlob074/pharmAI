c5eda365893fc7051a2c7ec4fd992008c48117a4
jjunlob074 <jjunlob074@g.educaand.es>
2024-06-04 01:27:23 +0200

    corrijo bug reminders multiusuario

--- src/pages/api/reminders.ts
+++ src/pages/api/reminders.ts
@@ -5,11 +5,12 @@ export async function GET({ request }) {
   const pastReminders = await db.select().from(MedicationReminder).where(lte(MedicationReminder.time, now));
   if (pastReminders.length > 0) {
     await db.delete(MedicationReminder).where(lte(MedicationReminder.time, now));
-    return new Response(JSON.stringify({ reload: true }), {
+    const alertMessages = pastReminders.map(reminder => `Reminder that has expired: "${reminder.description}" - "${reminder.medication}"`);
+    return new Response(JSON.stringify({ reload: true, alertMessages }), {
       headers: { 'Content-Type': 'application/json' },
     });
   }
-  return new Response(JSON.stringify({ reload: false }), {
+  return new Response(JSON.stringify({ reload: false, alertMessages: [] }), {
     headers: { 'Content-Type': 'application/json' },
   });
 }
