3eb719fc39f7ba8f0ff059495acb542bfb555f8c
jjunlob074 <jjunlob074@g.educaand.es>
2024-06-16 19:52:05 +0200

    añado documentacion

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,14 @@ import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import { defineConfig } from "astro/config";
 import node from '@astrojs/node';
-
 import db from "@astrojs/db";
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://astro-moon-landing.netlify.app/",
-  integrations: [tailwind(), icon(), db()],
+  integrations: [tailwind(), icon(), db(), mdx()],
   vite: {
     optimizeDeps: {
       exclude: ["oslo"]
